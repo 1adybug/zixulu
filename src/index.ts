594ec2f592a7da83626aff28634001e23513a5ac
luzixu <lurongv@qq.com>
2023-12-22 17:37:51 +0800

    迁移到 create-father

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+import { Argument, Command } from "commander"
+import { consola } from "consola"
+import { addPrettierConfig, readPackageJson, removeComment, tailwind, vite } from "./utils"
+
+const program = new Command()
+
+const pkg = readPackageJson("./")
+
+program.name("格数科技").version(pkg.version)
+
+program
+    .command("prettier")
+    .description("添加 prettier 配置文件")
+    .option("-t, --tailwind", "是否添加 tailwind 插件")
+    .action(options => {
+        const { tailwind } = options
+        addPrettierConfig(tailwind)
+    })
+
+program.command("vite").description("删除 vite 模板中的某些配置").action(vite)
+
+program.command("tailwind").description("添加 tailwind 配置文件").action(tailwind)
+
+program.command("remove-comment").description("删除所有注释").addArgument(new Argument("path")).action(removeComment)
+
+program.parse()
