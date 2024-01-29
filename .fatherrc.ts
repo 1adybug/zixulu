c87151ad52e794fa60a6134d745f616b9eee6c4d
luzixu <lurongv@qq.com>
2024-01-29 10:15:01 +0800

    添加多项功能

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "father"
 
 export default defineConfig({
-    cjs: { output: "dist" },
+    cjs: {
+        output: "dist"
+    },
+    targets: {
+        node: 18,
+        chrome: 90
+    },
     sourcemap: true
 })
