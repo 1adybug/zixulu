77ae73387fe3822f1ec1f0bdea7daa1fd5a70be4
陆子虚 <11374106+zixulu@user.noreply.gitee.com>
2024-03-04 14:34:14 +0800

    修复一些 bug

---
+++ src/constant/index.ts
@@ -0,0 +1,11 @@
+export enum Manager {
+    npm = "npm",
+    yarn = "yarn",
+    pnpm = "pnpm"
+}
+
+export enum Registry {
+    npm = "https://registry.npmjs.org/",
+    taobao = "https://registry.npmmirror.com/",
+    tencent = "https://mirrors.cloud.tencent.com/npm/"
+}
