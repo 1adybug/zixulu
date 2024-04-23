d1cc546b8a4d9a185636255f0997ae4597ec9ad9
陆子虚 <11374106+zixulu@user.noreply.gitee.com>
2024-04-23 11:01:21 +0800

    ✨feature: 新增 antd 命令

--- src/index.ts
+++ src/index.ts
@@ -762,4 +762,76 @@ program
         await spawnAsync("npx prisma db push && npx prisma generate")
     })
 
+program
+    .command("antd")
+    .description("添加 antd 配置")
+    .action(async () => {
+        const packageJson = await readPackageJson()
+        await addDependencies(packageJson, "@ant-design/cssinjs")
+        await addDependencies(packageJson, "@ant-design/icons")
+        await addDependencies(packageJson, "ahooks")
+        await addDependencies(packageJson, "antd")
+        const dir = await readdir("./")
+        const componentDir = dir.includes("src") ? "src/components" : "components"
+        await mkdir(componentDir, { recursive: true })
+        if (packageJson.dependencies.next) {
+            await addDependencies(packageJson, "@ant-design/nextjs-registry")
+            await writeFile(
+                join(componentDir, "AntdNextRegistry.tsx"),
+                `"use client"
+import { StyleProvider } from "@ant-design/cssinjs"
+import { AntdRegistry } from "@ant-design/nextjs-registry"
+import { ConfigProvider } from "antd"
+import zhCN from "antd/locale/zh_CN"
+import { FC, ReactNode } from "react"
+
+export type AntdNextRegistryProps = {
+    children?: ReactNode
+}
+
+const AntdNextRegistry: FC<AntdNextRegistryProps> = props => {
+    const { children } = props
+
+    return (
+        <AntdRegistry>
+            <ConfigProvider locale={zhCN}>
+                <StyleProvider hashPriority="high">{children}</StyleProvider>
+            </ConfigProvider>
+        </AntdRegistry>
+    )
+}
+
+export default AntdNextRegistry
+`
+            )
+        } else {
+            await writeFile(
+                join(componentDir, "AntdRegistry.tsx"),
+                `"use client"
+import { StyleProvider } from "@ant-design/cssinjs"
+import { ConfigProvider } from "antd"
+import zhCN from "antd/locale/zh_CN"
+import { FC, ReactNode } from "react"
+
+export type AntdRegistryProps = {
+    children?: ReactNode
+}
+
+const AntdRegistry: FC<AntdRegistryProps> = props => {
+    const { children } = props
+
+    return (
+        <ConfigProvider locale={zhCN}>
+            <StyleProvider hashPriority="high">{children}</StyleProvider>
+        </ConfigProvider>
+    )
+}
+
+export default AntdRegistry
+`
+            )
+        }
+        await writePackageJson(packageJson)
+    })
+
 program.parse()
