080a96170903d45e694c23374439803cb7d6699c
陆子虚 <lurongv@qq.com>
2024-05-08 14:41:52 +0800

    ✨feature: rm-git 命令不要设置是否是文件夹

--- src/utils/removeFileOrFolderFromGit.ts
+++ src/utils/removeFileOrFolderFromGit.ts
@@ -1,12 +1,10 @@
 import consola from "consola"
+import { stat } from "fs/promises"
 import { backupFirst, execAsync } from "."
 
-export type RemoveFileOrFolderFromGitOptions = {
-    recursive?: boolean
-}
-
-export async function removeFileOrFolderFromGit(path: string, options?: RemoveFileOrFolderFromGitOptions) {
-    const { recursive = false } = options || {}
+export async function removeFileOrFolderFromGit(path: string) {
+    const stats = await stat(path)
+    const recursive = stats.isDirectory()
     await backupFirst(true)
     consola.start(`开始从 git 中删除 ${path}`)
     await execAsync(`git filter-branch --force --index-filter "git rm${recursive ? " -r" : ""} --cached --ignore-unmatch ${path}" --prune-empty --tag-name-filter cat -- --all`)
