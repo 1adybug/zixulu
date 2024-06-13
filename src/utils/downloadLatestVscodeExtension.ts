4d28e9a10d5604c11c0cbdffe32a853a7826af3e
陆子虚 <lurongv@qq.com>
2024-06-13 09:37:13 +0800

    ✨feature: 更新了 scode 下载

--- src/utils/downloadLatestVscodeExtension.ts
+++ src/utils/downloadLatestVscodeExtension.ts
@@ -1,11 +1,18 @@
 import consola from "consola"
-import { mkdir } from "fs/promises"
+import { copyFile, mkdir } from "fs/promises"
+import { homedir } from "os"
+import { join } from "path"
 import { downloadVscodeExts, writeInstallVscodeExtScript } from "."
 
 export async function downloadLatestVscodeExtension() {
     consola.start("开始下载最新的 vscode 插件")
+    const userDir = homedir()
+    const snippet = join(userDir, "AppData/Roaming/Code/User/snippets/global.code-snippets")
+    const setting = join(userDir, "AppData/Roaming/Code/User/settings.json")
     const dir = `vscode-${Date.now()}`
     await mkdir(dir, { recursive: true })
+    await copyFile(snippet, join(dir, "global.code-snippets"))
+    await copyFile(setting, join(dir, "settings.json"))
     await downloadVscodeExts(dir)
     await writeInstallVscodeExtScript(dir)
     consola.success("下载最新的 vscode 插件完成")
