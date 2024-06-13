a9d1d48d441898f825ec8fcfe9a2e3207d99d97e
陆子虚 <lurongv@qq.com>
2024-06-13 11:00:04 +0800

    ✨feature: 改进 scode 下载

--- src/utils/downloadLatestVscodeExtension.ts
+++ src/utils/downloadLatestVscodeExtension.ts
@@ -2,19 +2,30 @@ import consola from "consola"
 import { copyFile, mkdir } from "fs/promises"
 import { homedir } from "os"
 import { join } from "path"
-import { downloadVscode, downloadVscodeExts, writeInstallVscodeExtScript } from "."
+import { downloadVscodeExts, writeInstallVscodeExtScript } from "."
 
 export async function downloadLatestVscodeExtension() {
-    consola.start("开始下载最新的 vscode 插件")
+    const { default: inquirer } = await import("inquirer")
     const userDir = homedir()
     const snippet = join(userDir, "AppData/Roaming/Code/User/snippets/global.code-snippets")
     const setting = join(userDir, "AppData/Roaming/Code/User/settings.json")
     const dir = `vscode-${Date.now()}`
     await mkdir(dir, { recursive: true })
+    consola.start("开始下载最新 VSCode 配置")
     await copyFile(snippet, join(dir, "global.code-snippets"))
     await copyFile(setting, join(dir, "settings.json"))
-    await downloadVscode(dir)
+    consola.success("下载最新 VSCode 配置完成")
     await downloadVscodeExts(dir)
+    consola.success("下载最新 VSCode 插件完成")
     await writeInstallVscodeExtScript(dir)
-    consola.success("下载最新的 vscode 插件完成")
+    const { downloadVscode } = await inquirer.prompt({
+        type: "confirm",
+        name: "downloadVscode",
+        message: "是否下载最新版 VSCode"
+    })
+    if (downloadVscode) {
+        consola.start("开始下载最新 VSCode")
+        await downloadVscode(dir)
+        consola.success("下载最新 VSCode 完成")
+    }
 }
