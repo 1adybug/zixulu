99b2525ef4387f708484ad1a45ddecfc8a4b83d9
陆子虚 <lurongv@qq.com>
2024-06-24 16:00:56 +0800

    ✨feature: 合并添加依赖

--- src/utils/addAntd.ts
+++ src/utils/addAntd.ts
@@ -5,9 +5,7 @@ import { addDependencies, readPackageJson } from "."
 
 export async function addAntd() {
     consola.start("开始添加 antd 配置")
-    await addDependencies("@ant-design/cssinjs")
-    await addDependencies("@ant-design/icons")
-    await addDependencies("antd")
+    await addDependencies(["@ant-design/cssinjs", "@ant-design/icons", "antd"])
     const dir = await readdir("./")
     const componentDir = dir.includes("src") ? "src/components" : "components"
     await mkdir(componentDir, { recursive: true })
