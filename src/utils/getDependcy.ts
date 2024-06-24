d8c247cf6758058410d52198ad4aa9becd6579da
陆子虚 <lurongv@qq.com>
2024-06-24 17:17:43 +0800

    ✨feature: 更新 rsbuild 初始化逻辑

---
+++ src/utils/getDependcy.ts
@@ -0,0 +1,6 @@
+import { readPackageJson } from "."
+
+export async function getDependcy(name: string) {
+    const packageJson = await readPackageJson()
+    return packageJson.dependencies?.[name] || packageJson.devDependencies?.[name] || packageJson.peerDependencies?.[name] || packageJson.optionalDependencies?.[name]
+}
