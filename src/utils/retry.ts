0b4ceb7b0f262b79f0fb312552760556c7304b51
陆子虚 <lurongv@qq.com>
2024-07-23 10:03:55 +0800

    🐞 fix: 修复了 count 没有自减的问题

--- src/utils/retry.ts
+++ src/utils/retry.ts
@@ -8,7 +8,7 @@ export async function retry<T>(config: RetryConfig<T>): Promise<T>
 export async function retry<T>(action: () => Promise<T>, count?: number): Promise<T>
 export async function retry<T>(actionOrConfig: RetryConfig<T> | (() => Promise<T>), countNumber?: number) {
     let current = 1
-    const { action, count = 2, callback } = typeof actionOrConfig === "function" ? ({ action: actionOrConfig, count: countNumber } as RetryConfig<T>) : actionOrConfig
+    let { action, count = 2, callback } = typeof actionOrConfig === "function" ? ({ action: actionOrConfig, count: countNumber } as RetryConfig<T>) : actionOrConfig
     async function _retry() {
         try {
             return await action()
@@ -16,6 +16,7 @@ export async function retry<T>(actionOrConfig: RetryConfig<T> | (() => Promise<T
             callback?.(error, current)
             if (count === 1) throw error
             current++
+            count--
             return await _retry()
         }
     }
