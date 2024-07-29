bf43f9d4e49a59863358d5e5a54906cebc91de8d
陆子虚 <lurongv@qq.com>
2024-07-29 14:25:09 +0800

    🐞 fix: 修复 addDependcy 没有 await 的问题

--- src/utils/reinstall.ts
+++ src/utils/reinstall.ts
@@ -1,6 +1,12 @@
-import { spawnAsync } from "soda-nodejs"
+import { rm } from "fs/promises"
+import { getPackageManager } from "./getPackageManager"
+import { installDependceny } from "./installDependceny"
 
-export async function reinstall(name: string) {
-    await spawnAsync(`yarn remove ${name}`, { shell: true, stdio: "inherit" })
-    await spawnAsync(`yarn add ${name}`, { shell: true, stdio: "inherit" })
+export async function reinstall() {
+    const manager = await getPackageManager()
+    await rm("node_modules", { recursive: true, force: true })
+    await rm("pnpm-lock.yaml", { force: true })
+    await rm("yarn.lock", { force: true })
+    await rm("package-lock.json", { force: true })
+    await installDependceny({ manager, silent: true })
 }
