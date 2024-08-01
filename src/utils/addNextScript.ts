fbf59b08405f70e13339ce8af5f28c4e94dc7b7d
陆子虚 <lurongv@qq.com>
2024-08-01 19:33:05 +0800

    ✨feature: 优化细节

--- src/utils/addNextScript.ts
+++ src/utils/addNextScript.ts
@@ -6,12 +6,15 @@ import { installDependceny } from "./installDependceny"
 import { readPackageJson } from "./readPackageJson"
 import { writePackageJson } from "./writePackageJson"
 
-const script = `import { readFile } from "fs/promises"
+const script = `import { config } from "dotenv"
+import { readFile } from "fs/promises"
 import { checkPort } from "get-port-please"
 import { createServer as createHttpServer } from "http"
 import { createServer as createHttpsServer } from "https"
 import next from "next"
 import { join } from "path"
+
+config()
 
 async function main() {
     const PEM_PATH = process.env.PEM_PATH
@@ -32,7 +35,7 @@ main()`
 
 export async function addNextScript() {
     await addDependency({
-        package: ["cross-env", "get-port-please"],
+        package: ["dotenv", "get-port-please"],
         type: "devDependencies"
     })
     await mkdir("scripts", { recursive: true })
@@ -41,9 +44,9 @@ export async function addNextScript() {
     consola.success(`已添加 scripts/start${existed ? "2" : ""}.js`)
     const packageJson = await readPackageJson()
     packageJson.scripts ??= {}
-    if (packageJson.scripts.start) packageJson.scripts.start2 = `cross-env PEM_PATH="" PORT="" node scripts/start${existed ? "2" : ""}.js`
+    if (packageJson.scripts.start) packageJson.scripts.start2 = `cross-env node scripts/start${existed ? "2" : ""}.js`
     else packageJson.scripts.start = `cross-env PEM_PATH="" PORT="" node scripts/start${existed ? "2" : ""}.js`
     await writePackageJson({ data: packageJson })
-    consola.success(`已添加启动命令 start${existed ? "2" : ""}`)
+    consola.success(`已添加启动命令 start${packageJson.scripts.start ? "2" : ""}`)
     await installDependceny()
 }
