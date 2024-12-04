379289a752f94f27d2ca76c28fc9ad149669a5b4
陆子虚 <11374106+zixulu@user.noreply.gitee.com>
2024-12-04 17:48:04 +0800

    ✨feature: 添加 prettier 配置文件

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,3 +1,23 @@
+// @ts-check
+
+import { parse } from "path"
+import { globSync } from "glob"
+
+const jsExts = [".js", ".jsx", ".ts", ".tsx", ".cjs", ".mjs", ".cts", ".mts", ".vue"]
+
+const assetExts = Array.from(
+    new Set(
+        globSync("**/*", { ignore: ["node_modules/**"], withFileTypes: true })
+            .filter(path => path.isFile() && !jsExts.some(ext => path.name.toLowerCase().endsWith(ext)))
+            .map(path => parse(path.name).ext.slice(1))
+            .filter(ext => ext !== ""),
+    ),
+)
+
+const assetExtsRegStr = `\\.(${assetExts.join("|")}|${assetExts.join("|").toUpperCase()})`
+
+const assetQueryRegStr = "(\\?[a-zA-Z0-9]+)?"
+
 /**
  * @type {import("prettier").Options}
  */
@@ -6,7 +26,23 @@ const config = {
     tabWidth: 4,
     arrowParens: "avoid",
     printWidth: 160,
-    plugins: ["prettier-plugin-organize-imports"],
+    plugins: ["@ianvs/prettier-plugin-sort-imports"],
+    importOrder: [
+        "<BUILTIN_MODULES>",
+        "",
+        "<THIRD_PARTY_MODULES>",
+        "",
+        `^@.+?(?<!${assetExtsRegStr}${assetQueryRegStr})$`,
+        "",
+        `^\\.{1,2}/.+?(?<!${assetExtsRegStr}${assetQueryRegStr})$`,
+        "",
+        `^@.+?${assetExtsRegStr}${assetQueryRegStr}$`,
+        "",
+        `^\\.{1,2}/.+?${assetExtsRegStr}${assetQueryRegStr}$`,
+    ],
+    importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+    importOrderTypeScriptVersion: "5.0.0",
+    importOrderCaseSensitive: true,
 }
 
 export default config
