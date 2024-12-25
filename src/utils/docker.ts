f91e26cc9b9c7e41d69343768cb7971d507c355c
陆子虚 <lurongv@qq.com>
2024-12-25 15:06:47 +0800

    ✨ feature: 添加 Docker 安装和配置功能，支持自定义镜像地址

--- src/utils/docker.ts
+++ src/utils/docker.ts
@@ -1,7 +1,10 @@
+import { mkdir, readdir, writeFile } from "fs/promises"
 import { JSDOM } from "jsdom"
 import { execAsync } from "soda-nodejs"
 
 export async function docker() {
+    const isRoot = !!process.env.SUDO_USER
+    if (!isRoot) throw new Error("请使用 root 用户执行命令")
     const info = await execAsync("lsb_release -a")
     const distributorId = info.match(/Distributor ID:\s+(.+)/)?.[1]
     if (distributorId !== "Ubuntu") throw new Error("暂不支持 Ubuntu 以外的系统")
@@ -17,6 +20,31 @@ export async function docker() {
     const deb4 = hrefs.findLast(item => item.startsWith("docker-buildx-plugin_"))!
     const deb5 = hrefs.findLast(item => item.startsWith("docker-compose-plugin_"))!
     const debs = [deb, deb2, deb3, deb4, deb5]
-    await Promise.all(debs.map(deb => execAsync(`wget -q https://download.docker.com/linux/ubuntu/dists/${codename}/pool/stable/amd64/${deb}`)))
-    await execAsync(`sudo dpkg -i ${debs.join(" ")}`)
+    const dir = await readdir(".")
+    await Promise.all(
+        debs
+            .filter(deb => !dir.includes(deb))
+            .map(deb => execAsync(`wget -q https://download.docker.com/linux/ubuntu/dists/${codename}/pool/stable/amd64/${deb}`)),
+    )
+    await execAsync(`dpkg -i ${debs.join(" ")}`)
+    await mkdir("/etc/docker", { recursive: true })
+    const daemon = {
+        "registry-mirrors": ["https://docker.sunzishaokao.com", "https://hub.hxui.site", "https://docker.1ms.run"],
+        "exec-opts": ["native.cgroupdriver=systemd"],
+    }
+    const { default: inquirer } = await import("inquirer")
+    type Answer = {
+        mirrors: string
+    }
+    const { mirrors } = await inquirer.prompt<Answer>([
+        {
+            type: "input",
+            name: "mirrors",
+            message: "请输入镜像地址，多个用逗号分隔，留空则跳过",
+        },
+    ])
+    daemon["registry-mirrors"].push(...mirrors.split(/,，/))
+    await writeFile("/etc/docker/daemon.json", JSON.stringify(daemon, null, 4))
+    await execAsync("systemctl daemon-reload")
+    await execAsync("systemctl restart docker")
 }
