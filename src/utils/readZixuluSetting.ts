15e39c25fe4520af15938e098ff1c8ea7d3ceea3
陆子虚 <lurongv@qq.com>
2025-01-09 13:23:21 +0800

    ✨ feat: 添加同步 VSCode snippets 功能，支持文件替换和备份选项

--- src/utils/readZixuluSetting.ts
+++ src/utils/readZixuluSetting.ts
@@ -3,10 +3,17 @@ import { readFile } from "fs/promises"
 import { homedir } from "os"
 import { join } from "path"
 
+export type SyncVscodeSnippets = {
+    replace?: boolean
+    filename?: string
+    backup?: boolean
+}
+
 export type ZixuluSetting = {
     vscodeDownloadHistory?: string[]
     softwareDownloadHistory?: string[]
     upgradeWorkspaceDependcenyHistory?: Record<string, string[]>
+    syncVscodeSnippets?: SyncVscodeSnippets
 }
 
 export async function readZixuluSetting(): Promise<ZixuluSetting> {
