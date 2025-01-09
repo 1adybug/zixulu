c35146d5d1366b84e2d86e2e94551fadb4374b5f
陆子虚 <lurongv@qq.com>
2025-01-09 16:56:06 +0800

    🐞 fix: 修复文件名保存错误的问题

--- src/utils/syncSnippets.ts
+++ src/utils/syncSnippets.ts
@@ -40,7 +40,7 @@ export async function syncSnippets() {
             message: "请输入新的文件名（不带.code-snippets）",
             default: setting.syncVscodeSnippets?.filename?.replace(/\.code-snippets$/, ""),
         })
-        setting = { ...setting, syncVscodeSnippets: { ...setting.syncVscodeSnippets, filename: newFilename } }
+        setting = { ...setting, syncVscodeSnippets: { ...setting.syncVscodeSnippets, filename: `${newFilename.replace(/\.code-snippets$/, "")}.code-snippets` } }
         filename = `${newFilename.replace(/\.code-snippets$/, "")}.code-snippets`
     }
     if (existsSync(join(snippetSource, filename))) {
