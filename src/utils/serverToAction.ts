927a5bfd30e260b78584f845c1f78f04011b56cf
陆子虚 <lurongv@qq.com>
2025-02-12 08:54:10 +0800

    ✨feature: 更新 serverToAction 函数，过滤掉不导出的文件

--- src/utils/serverToAction.ts
+++ src/utils/serverToAction.ts
@@ -1,13 +1,19 @@
-import { mkdir, readdir, rm, writeFile } from "fs/promises"
+import { mkdir, readFile, readdir, rm, writeFile } from "fs/promises"
 import { join } from "path"
 
 export async function serverToAction() {
     const dir = await readdir("server")
     await rm("actions", { recursive: true, force: true })
     const actions = dir.filter(item => item.toLowerCase().endsWith(".ts"))
+    const actions2: string[] = []
+    for (const item of actions) {
+        const content = await readFile(join("server", item), "utf-8")
+        if (/^"no export"$/m.test(content)) continue
+        actions2.push(item)
+    }
     await mkdir("actions", { recursive: true })
     await Promise.all(
-        actions.map(async item => {
+        actions2.map(async item => {
             const base = item.replace(/\.ts$/i, "")
             await writeFile(
                 join("actions", item),
