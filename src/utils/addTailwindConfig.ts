d8eef6b62a219cbeca2edcf9379e651f5c7e84eb
陆子虚 <lurongv@qq.com>
2025-04-11 15:16:50 +0800

    ✨feature: 优化配置

--- src/utils/addTailwindConfig.ts
+++ src/utils/addTailwindConfig.ts
@@ -1,6 +1,8 @@
 import { writeFile } from "fs/promises"
 import consola from "consola"
 
+import { readPackageJson } from "./readPackageJson"
+
 /** Tailwind 默认配置内容 */
 export const tailwindConfig = `import { Config } from "tailwindcss"
 
@@ -11,11 +13,37 @@ const config: Config = {
         "./src/**/*.{js,ts,jsx,tsx}",
         "./app/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./routes/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
         extend: {},
     },
     plugins: [],
+}
+
+export default config
+`
+
+export const tailwindConfigWithHeroUi = `import { heroui } from "@heroui/react"
+import { Config } from "tailwindcss"
+
+const config: Config = {
+    content: [
+        "./index.html",
+        "./public/index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "./app/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./routes/**/*.{js,ts,jsx,tsx}",
+        "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    darkMode: "class",
+    plugins: [heroui()],
 }
 
 export default config
@@ -27,7 +55,10 @@ export default config
  */
 export async function addTailwindConfig() {
     try {
-        await writeFile("tailwind.config.ts", tailwindConfig, "utf-8")
+        const packageJson = await readPackageJson()
+        const dependencies = packageJson.dependencies
+        const config = dependencies?.["@heroui/react"] ? tailwindConfigWithHeroUi : tailwindConfig
+        await writeFile("tailwind.config.ts", config, "utf-8")
         consola.success("添加 tailwind.config.ts 配置成功")
     } catch (error) {
         consola.fail("添加 tailwind.config.ts 配置失败")
