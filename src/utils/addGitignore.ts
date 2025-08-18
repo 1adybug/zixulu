74ab9c11173ee896bd4b61b5ae2362bb49767817
陆子虚 <lurongv@qq.com>
2025-08-18 10:38:26 +0800

    ✨feature: 添加 gitignore 时顺便设置换行符

--- src/utils/addGitignore.ts
+++ src/utils/addGitignore.ts
@@ -1,10 +1,13 @@
 import { addedRules } from "@constant/index"
 
+import { addGitAttributes } from "./addGitAttributes"
 import { addRuleToGitIgnore } from "./addRuleToGitIgnore"
 
 /**
  * 添加预设的 gitignore 规则
  */
 export async function addGitignore() {
-    return addRuleToGitIgnore(...addedRules)
+    await addGitAttributes()
+    const message = await addRuleToGitIgnore(...addedRules)
+    return message
 }
