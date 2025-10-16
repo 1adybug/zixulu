17d36925d401a896fb16e3fee14f35129ce5fd7a
陆子虚 <lurongv@qq.com>
2025-10-16 14:46:25 +0800

    ✨feature: 使用全新的 prettier 插件

--- src/utils/addPrettier.ts
+++ src/utils/addPrettier.ts
@@ -5,25 +5,7 @@ import { AddDependenciesConfig, addDependency } from "./addDependency"
 import { hasDependency } from "./hasDependency"
 import { installDependceny } from "./installDependceny"
 import { readPackageJson } from "./readPackageJson"
-import { readTsConfig } from "./readTsConfig"
 import { writePackageJson } from "./writePackageJson"
-
-const originalConfig = `// @ts-check
-
-/**
- * @type {import("prettier").Options}
- */
-const config = {
-    semi: false,
-    tabWidth: 4,
-    arrowParens: "avoid",
-    printWidth: 160,
-    endOfLine: "lf",
-    plugins: ["prettier-plugin-organize-imports"],
-}
-
-export default config
-`
 
 const ignoreConfig = `node_modules
 public
@@ -35,91 +17,7 @@ generated
 .generated
 `
 
-/**
- * prettier 配置生成器参数
- */
-export type GetPrettierConfigParams = {
-    /** 是否使用 tailwind */
-    tailwind: boolean
-    /** 是否使用了本地 @images 类似的别名 */
-    atAlias: boolean
-    /** 是否是 next 项目 */
-    next: boolean
-    /** 是否是 react 项目 */
-    react: boolean
-}
-
-/**
- * 生成 prettier 配置文件内容
- * @param params 配置参数
- */
-export function getPrettierConfig({ tailwind, atAlias, next, react }: GetPrettierConfigParams) {
-    const plugins = ["@ianvs/prettier-plugin-sort-imports"]
-
-    if (tailwind) plugins.push("prettier-plugin-tailwindcss")
-
-    const prettierConfigText = `// @ts-check
-${
-    atAlias
-        ? `
-import { readFileSync } from "fs"`
-        : ""
-}
-import { parse } from "path"
-import { globSync } from "glob"
-
-/**
- * 数组去重
- * @template T - 数组的元素类型
- * @param {T[]} array - 输入的数组
- * @return {T[]} 新数组
- */
-function unique(array) {
-    return Array.from(new Set(array))
-}
-
-const jsExts = [".js", ".jsx", ".ts", ".tsx", ".cjs", ".mjs", ".cts", ".mts", ".vue"]
-
-const assetExts = unique(
-    globSync("**/*", { ignore: ["node_modules/**"${next ? `, ".next/**"` : ""}], withFileTypes: true, cwd: import.meta.dirname })
-        .filter(path => path.isFile() && !jsExts.some(ext => path.name.toLowerCase().endsWith(ext)))
-        .map(path => parse(path.name).ext.slice(1))
-        .filter(ext => ext !== ""),
-)
-
-const assetExtsRegStr = \`\\\\.(\${assetExts.join("|")}|\${assetExts.join("|").toUpperCase()})\`
-
-const assetQueryRegStr = "(\\\\?[a-zA-Z0-9]+)?"
-${
-    atAlias
-        ? `
-const namespaces = unique(
-    unique(
-        globSync("**/package.json", { withFileTypes: true, cwd: import.meta.dirname })
-            .filter(path => path.isFile())
-            .map(path => path.fullpath()),
-    )
-        .map(path => JSON.parse(readFileSync(path, "utf8")))
-        .map(json =>
-            Object.keys({
-                ...json.dependencies,
-                ...json.devDependencies,
-                ...json.peerDependencies,
-                ...json.optionalDependencies,
-            }),
-        )
-        .flat()
-        .filter(dep => dep.startsWith("@"))
-        .map(dep => dep.split("/")[0].slice(1)),
-)
-`
-        : ""
-}
-const folders = unique(
-    globSync("**/*", { withFileTypes: true, cwd: import.meta.dirname, ignore: ["node_modules/**"${next ? `, ".next/**"` : ""}] })
-        .filter(path => path.isDirectory())
-        .map(path => path.name),
-).sort()
+const prettierConfig = `// @ts-check
 
 /**
  * @type {import("prettier").Options}
@@ -130,34 +28,126 @@ const config = {
     arrowParens: "avoid",
     printWidth: 160,
     endOfLine: "lf",
-    plugins: [${plugins.map(plugin => `"${plugin}"`).join(", ")}],
-    importOrder: [${
-        react
-            ? `
-        "^react(/.+)?$",
-        "^react-dom(/.+)?$",
-        "^react-native(/.+)?$",`
-            : ""
-    }
-        "<BUILTIN_MODULES>",
-        ${atAlias ? `\`^@(\${namespaces.join("|")})/\`,` : `"^@[^/]",`}
-        "<THIRD_PARTY_MODULES>",
-        ...folders.map(folder => ["", \`^@/?\${folder}(.+?(?<!\${assetExtsRegStr}\${assetQueryRegStr}))?$\`]).flat(),
-        "",
-        \`^@/.+?(?<!\${assetExtsRegStr}\${assetQueryRegStr})$\`,
-        \`^\\\\.{1,2}/.+?(?<!\${assetExtsRegStr}\${assetQueryRegStr})$\`,
-        "",
-        \`^@/.+?\${assetExtsRegStr}\${assetQueryRegStr}$\`,
-        \`^\\\\.{1,2}/.+?\${assetExtsRegStr}\${assetQueryRegStr}$\`,
-    ],
-    importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
-    importOrderTypeScriptVersion: "5.0.0",
-    importOrderCaseSensitive: true,
+    plugins: ["./prettier-plugin-sort-imports.mjs"],
 }
 
 export default config
 `
-    return prettierConfigText
+
+/**
+ * prettier 配置生成器参数
+ */
+export interface GetPluginConfigParams {
+    /** 是否使用 tailwind */
+    isTailwind: boolean
+    /** 是否是 react 项目 */
+    isReact: boolean
+}
+
+function getPluginConfig({ isTailwind, isReact }: GetPluginConfigParams) {
+    const config = `// @ts-check
+
+import { readFileSync } from "fs"
+import { builtinModules } from "module"
+
+import { createPlugin } from "@1adybug/prettier-plugin-sort-imports"
+import JSON5 from "json5"
+import blockPadding from "prettier-plugin-block-padding"${
+        isTailwind
+            ? `
+import * as tailwindcss from "prettier-plugin-tailwindcss"`
+            : ""
+    }
+${
+    isReact
+        ? `
+/**
+ * @param {string} path
+ */
+function isReact(path) {
+    return /^@?react\\b/.test(path)
+}
+`
+        : ""
+}
+/**
+ * @param {string} path
+ */
+function isBuiltin(path) {
+    return path.startsWith("node:") || builtinModules.includes(path)
+}
+
+/** @type {string[]} */
+let pathAlias = []
+
+try {
+    const tsConfig = JSON5.parse(readFileSync("tsconfig.json", "utf-8"))
+    pathAlias = Object.keys(tsConfig.compilerOptions?.paths ?? {})
+        .map(item => item.match(/^(@.*\\/)\\*/))
+        .filter(Boolean)
+        .map(item => /** @type {string} */ (item?.[1]))
+} catch {}
+
+/**
+ * @param {string} path
+ */
+function isAbsolute(path) {
+    return pathAlias.some(item => path.startsWith(item))
+}
+
+/**
+ * @param {string} path
+ */
+function isRelative(path) {
+    return path.startsWith("./") || path.startsWith("../")
+}
+
+/**
+ * @param {string} a
+ * @param {string} b
+ */
+function compareGroupName(a, b) {
+    const orders = [${isReact ? `"react", ` : ""}"builtin", "third-party", "absolute", "relative"]
+
+    a = a.replace(/-side-effect$/, "")
+    b = b.replace(/-side-effect$/, "")
+    return orders.indexOf(a) - orders.indexOf(b) || a.localeCompare(b)
+}
+
+export default createPlugin({
+    getGroup({ path, isSideEffect }) {
+        if (isSideEffect) {${
+            isReact
+                ? `
+            if (isReact(path)) return "react-side-effect"`
+                : ""
+        }
+            if (isBuiltin(path)) return "builtin-side-effect"
+            if (isAbsolute(path)) return "absolute-side-effect"
+            if (isRelative(path)) return "relative-side-effect"
+            return "third-party-side-effect"
+        }
+${
+    isReact
+        ? `
+        if (isReact(path)) return "react"`
+        : ""
+}
+        if (isBuiltin(path)) return "builtin"
+        if (isAbsolute(path)) return "absolute"
+        if (isRelative(path)) return "relative"
+        return "third-party"
+    },
+    sortGroup(a, b) {
+        return Number(a.isSideEffect) - Number(b.isSideEffect) || compareGroupName(a.name, b.name)
+    },
+    separator: "",
+    sortSideEffect: true,
+    removeUnusedImports: true,
+    otherPlugins: [blockPadding${isTailwind ? ", tailwindcss" : ""}],
+})
+`
+    return config
 }
 
 /**
@@ -167,29 +157,21 @@ export default config
 export async function addPrettier() {
     consola.start("开始添加 prettier 配置")
     const packageJson = await readPackageJson()
-    const tailwind =
+    const isTailwind =
         Object.keys(packageJson.dependencies ?? {}).includes("tailwindcss") || Object.keys(packageJson.devDependencies ?? {}).includes("tailwindcss")
-    let atAlias = false
-    try {
-        const config = await readTsConfig()
-        atAlias = Object.keys(config.compilerOptions?.paths ?? {}).some(path => /^@[a-zA-Z]/.test(path))
-    } catch {
-        /* empty */
-    }
-    const next = await hasDependency("next")
-    const react = await hasDependency("react")
-    await writeFile("prettier.config.mjs", originalConfig, "utf-8")
-    await writeFile(".prettierrc.mjs", getPrettierConfig({ tailwind, atAlias, next, react }), "utf-8")
+    const isReact = await hasDependency("react")
+    await writeFile("prettier-plugin-sort-imports.mjs", getPluginConfig({ isTailwind, isReact }), "utf-8")
+    await writeFile("prettier.config.mjs", prettierConfig, "utf-8")
     await writeFile(".prettierignore", ignoreConfig, "utf-8")
     const config2: AddDependenciesConfig = {
-        package: ["prettier", "@ianvs/prettier-plugin-sort-imports", "glob", "prettier-plugin-organize-imports"],
+        package: ["prettier", "@1adybug/prettier-plugin-sort-imports", "prettier-plugin-block-padding", "json5"],
         type: "devDependencies",
     }
-    if (tailwind) (config2.package as string[]).push("prettier-plugin-tailwindcss")
+    if (isTailwind) (config2.package as string[]).push("prettier-plugin-tailwindcss")
     await addDependency(config2)
     const packageJson2 = await readPackageJson()
     packageJson2.scripts ??= {}
-    packageJson2.scripts.format = "prettier --config prettier.config.mjs --write . && prettier --config .prettierrc.mjs --write ."
+    packageJson2.scripts.format = "prettier --write ."
     packageJson2.scripts.fg = 'npm run format && git add . && git commit -m "✨feature: format"'
     await writePackageJson({ data: packageJson2 })
     await installDependceny()
