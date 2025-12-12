ab96fecdf1dcbdd78f3c0229202254f05b8ed9a9
陆子虚 <lurongv@qq.com>
2025-12-12 08:41:06 +0800

    🐞 fix: 修复 heroui 的相关路径被错误添加的问题

--- src/utils/addTailwindConfig.ts
+++ src/utils/addTailwindConfig.ts
@@ -16,8 +16,7 @@ const config: Config = {
         "./components/**/*.{js,ts,jsx,tsx}",
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./routes/**/*.{js,ts,jsx,tsx}",
-        "./utils/**/*.{js,ts,jsx,tsx}",
-        "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+        "./utils/**/*.{js,ts,jsx,tsx}"
     ],
     theme: {
         extend: {},
