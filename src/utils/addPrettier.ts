c0ed14b98984c5c3404620e540318336fbd9e662
陆子虚 <lurongv@qq.com>
2026-01-16 14:12:59 +0800

    ✨feature: 优化 Prettier 规则

--- src/utils/addPrettier.ts
+++ src/utils/addPrettier.ts
@@ -35,7 +35,7 @@ const config = {
     plugins: ["@1adybug/prettier"],
     controlStatementBraces: "add",
     multiLineBraces: "add",
-    nodeProtocol: true,
+    nodeProtocol: "add",
 }
 
 export default config
