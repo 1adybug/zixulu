d52cb134c87509329be2fc38ac42f22a2bd1e187
陆子虚 <lurongv@qq.com>
2026-03-20 10:45:22 +0800

    🐞 fix: 修复 ESLint 错误

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,1 +1,14 @@
-export { default } from "@1adybug/eslint"
+// @ts-check
+
+import config from "@1adybug/eslint"
+import { defineConfig } from "eslint/config"
+
+export default defineConfig([
+    ...config,
+    {
+        rules: {
+            "n/hashbang": "off",
+            "n/no-process-exit": "off",
+        },
+    },
+])
